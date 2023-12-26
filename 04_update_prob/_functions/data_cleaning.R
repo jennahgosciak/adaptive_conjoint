07334c3d090628e51dc628e536db37ca30bf3477
Jennah Gosciak <52293028+jennahgosciak@users.noreply.github.com>
2023-12-26 13:20:17 -0500

    adding log of probabilities, removing stopifnot

--- 04_update_prob/_functions/data_cleaning.R
+++ 04_update_prob/_functions/data_cleaning.R
@@ -19,7 +19,7 @@ filter_test_data <- function(df) {
     filter(Status == "Survey Test")
 }
 
-clean_political_data <- function(df) {
+create_outcome_var_political <- function(df) {
   df %>%
     # = 1 if selecting the younger candidate
     # = 0 if selecting the older candidate
@@ -57,58 +57,164 @@ select_batch <- function(df) {
 
 # should be missing if participants do not consent
 check_consent <- function(df) {
-  df %>%
+  no_consent_num <- df %>%
     filter(Consent == "I do not consent to participate") %>%
-    mutate(Q2 = as.character(Q2)) %>%
-    distinct(`Q2`) %>%
-    is.na() %>%
-    stopifnot()
+    nrow()
+  
+  if (no_consent_num > 0) {
+    warning(str_glue("Dropping {no_consent_num} survey respondents who do not consent"))
+    
+    vars <- str_c("Q", 1:8)
+    no_consent_with_data <- df %>%
+      filter(Consent == "I do not consent to participate") %>%
+      select(all_of(vars)) %>%
+      is.na() %>%
+      rowSums() %>%
+      equals(0) %>%
+      sum()
+    
+    if (no_consent_with_data > 0) {
+      warning(str_glue("{no_consent_with_data} survey respondents who do not consent with non-missing responses"))
+    }
+    
+    df <- df %>%
+      filter(Consent != "I do not consent to participate")
+    
+    warning(str_glue("{nrow(df)} respondents in the data"))
+  }
+  return(df)
 }
 
 # should be missing if they are not in the US
 check_location_screen <- function(df) {
-  filtered_df <- df %>%
-    filter(PreScreen_Q1 != "Yes")
-
-  if (!all(is.na(filtered_df$Q1))) {
-    stop("Not all Q1 values are missing.")
+  no_prescreen_num <- df %>%
+    filter(PreScreen_Q1 != "Yes") %>%
+    nrow()
+  
+  if (no_prescreen_num > 0) {
+    warning(str_glue("Dropping {no_prescreen_num} survey respondents who are not in the US"))
+    
+    vars <- str_c("Q", 1:8)
+    no_prescreen_with_data <- df %>%
+      filter(PreScreen_Q1 != "Yes") %>%
+      select(all_of(vars)) %>%
+      is.na() %>%
+      rowSums() %>%
+      equals(0) %>%
+      sum()
+    
+    if (no_prescreen_with_data > 0) {
+      warning(str_glue("{no_prescreen_with_data} survey respondents who are not in the US with non-missing responses"))
+    }
+    
+    df <- df %>%
+      filter(PreScreen_Q1 == "Yes")
+    
+    warning(str_glue("{nrow(df)} respondents in the data"))
   }
+  return(df)
 }
 
 check_completion <- function(df) {
-  df %>%
+  completion_outcome <- df %>%
     select("Finished") %>%
     equals(TRUE) %>%
-    all() %>%
-    stopifnot()
+    all()
+  
+  if (completion_outcome != TRUE) {
+    num_respondents <- df %>%
+      filter(Finished != TRUE) %>%
+      nrow()
+    
+    # dropping respondents who did not finish
+    df <- df %>%
+      filter(Finished == TRUE)
+    
+    warning(str_glue("Dropping {num_respondents} survey respondents who did not finish"))
+    warning(str_glue("{nrow(df)} respondents in the data"))
+  }
+  return(df)
 }
 
 check_commitment <- function(df) {
-  df %>% 
-    mutate(Commitment_Q2 = str_to_lower(Commitment_Q2)) %>% 
-    distinct(Commitment_Q2) %>% 
-    equals('purple') %>% 
-    stopifnot()
+  commitment_check1 <- df %>%
+    distinct(Commitment_Q1) %>%
+    equals("Yes, I will") %>%
+    all()
   
-  df %>% 
-    distinct(Commitment_Q1) %>% 
-    equals('Yes, I will') %>% 
-    stopifnot()
+  if (commitment_check1 != TRUE | is.na(commitment_check1)) {
+    num_respondents <- df %>%
+      filter(Commitment_Q1 != "Yes, I will") %>%
+      nrow()
+    
+    warning(str_glue("{num_respondents} survey respondents did not pass commitment check 1; not dropping"))
+    warning(str_glue("{nrow(df)} respondents in the data"))
+  }
+  
+  commitment_check2 <- df %>%
+    mutate(Commitment_Q2 = str_to_lower(Commitment_Q2)) %>%
+    select(Commitment_Q2) %>%
+    equals("purple") %>%
+    all()
+  
+  if (commitment_check2 != TRUE | is.na(commitment_check2)) {
+    num_respondents <- df %>%
+      mutate(Commitment_Q2 = str_to_lower(Commitment_Q2)) %>%
+      filter(Commitment_Q2 != "purple") %>%
+      nrow()
+    
+    warning(str_glue("{num_respondents} survey respondents did not pass commitment check 2; not dropping"))
+    warning(str_glue("{nrow(df)} respondents in the data"))
+  }
 }
 
-create_profile_var_political <- function(df, pi) {
-  df %>%
-    # create 'profile' variable
-    mutate(profile = case_when(
-      rnum <= pi1 ~ 1,
-      rnum > pi1 & rnum <= pi2 ~ 2,
-      rnum > pi2 & rnum <= pi3 ~ 3,
-      rnum > pi3 & rnum <= pi4 ~ 4,
-      rnum > pi4 & rnum <= pi5 ~ 5,
-      rnum > pi5 & rnum <= pi6 ~ 6,
-      rnum > pi6 & rnum <= pi7 ~ 7,
-      rnum > pi7 ~ 8
-    ))
+check_pi_vars <- function(df, probabilities, num_contexts) {
+  check_pi <- df %>% 
+    select(all_of(str_c('pi', 1:num_contexts))) %>% 
+    distinct() %>% 
+    pivot_longer(everything(),
+                 names_to="Embedded data variable", values_to = "CDF_Data") %>% 
+    full_join(probabilities, by="Embedded data variable") %>% 
+    mutate(Comparison = CDF_Threshold == CDF_Data) %>% 
+    pull(Comparison) %>% 
+    equals(TRUE) %>% 
+    all()
+  
+  if (check_pi == FALSE) {
+    warning("Embedded data variables do not match updated probability in previous iteration")
+  }
+}
+
+create_context_var_political <- function(df) {
+    df %>%
+      # create profile context variable
+      mutate(
+        context = case_when(
+          rnum <= pi1 ~ 1,
+          rnum > pi1 & rnum <= pi2 ~ 2,
+          rnum > pi2 & rnum <= pi3 ~ 3,
+          rnum > pi3 & rnum <= pi4 ~ 4,
+          rnum > pi4 & rnum <= pi5 ~ 5,
+          rnum > pi5 & rnum <= pi6 ~ 6,
+          rnum > pi6 & rnum <= pi7 ~ 7,
+          rnum > pi7 ~ 8
+        ),
+        # create profile context label
+        context_label = case_when(
+          context == 1 ~ "white_female_high",
+          context == 2 ~ "white_female_low",
+          context == 3 ~ "black_female_high",
+          context == 4 ~ "black_female_low",
+          context == 5 ~ "black_male_high",
+          context == 6 ~ "black_male_low",
+          context == 7 ~ "white_male_high",
+          context == 8 ~ "white_male_low"
+        ),
+        context = factor(context,
+                         levels = c(1:8),
+                         ordered = TRUE
+        )
+      )
 }
 
 create_profile_var_jobs <- function(df, pi) {
