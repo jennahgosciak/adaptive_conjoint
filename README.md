a7c4e78901dfc7dfb9035391dff5468a396527c9
Daniel Molitor <molitdj97@gmail.com>
2025-04-18 17:51:32 -0400

    Add poststratification weights replication

--- README.md
+++ README.md
@@ -44,3 +44,36 @@ following Docker command:
 ```
 docker run --rm -v ./figures:/adaptive_conjoint/figures/ djmolitor/adaptive_conjoint /bin/bash -c "cd /adaptive_conjoint/ && Rscript code/immigrants_plots.R && Rscript code/job_applicants_plots.R"
 ```
+
+## Generating poststratification weights
+
+> [!NOTE]  
+> The post‑stratification weights used to generate all figures in this paper
+> are stored at `data/ipums_strata_sizes.RDS`. You don’t need to regenerate
+> them to reproduce the figures, but if you’d like to, follow the steps below.
+
+To re-generate the poststratification weights used in our analysis, you'll need
+an IPUMS API key. [Go here](https://account.ipums.org/api_keys) to generate a
+new key. Next, interactively execute the `code/download_ps_weights.R` script.
+The updated file should be in `data/ipums_strata_sizes.RDS`. To generate these
+weights, the script takes the following steps:
+
+1. Load data from IPUMS US via the API.
+    - Define the data extract. Sample is from US2022a. The variables we need are AGE, SEX, RACE, STATEFIP, HISPAN.
+    - Submit, wait, and then download the data from IPUMS
+2. Check AGE is an integer; rename to `age`.
+3. Create `race` variable in the following way (based on codebook values from [IPUMS website](https://usa.ipums.org/usa-action/variables/RACE#description_section)):
+    - RACE = 1, `race` = "White"
+    - RACE = 2, `race` = "Black or African American"
+    - RACE = 3, `race` = "American Indian or Alaska Native"
+    - RACED in the following list of values (400, 410, 420, 500, 600, 610, 620, 640, 641, 642, 643, 660, 661, 662, 663, 664, 665, 666, 667, 669, 670, 671, 673, 674, 675, 676, 677, 678, 679), `race` = "Asian". Note, these categories are: 400=Chinese, 410=Taiwanese, 420=Chinese and Taiwanese, 500=Japanese, 600=Filipino, 610=Asian Indian (Hindu 1920_1940), 620=Korean, 640=Vietnamese, 641=Bhutanese, 642=Mongolian, 643=Nepalese, 660=Cambodian, 661=Hmong, 662=Laotian, 663=Thai, 664=Bangladeshi, 665=Burmese, 666=Indonesian, 667=Malaysian, 669=Pakistani, 670=Sri Lankan, 671=All other Asian, n.e.c., 673=Chinese and Japanese, 674=Chinese and Filipino, 675=Chinese and Vietnamese, 676=Chinese and Asian write_in; Chinese and Other Asian, 677=Japanese and Filipino, 678=Asian Indian and Asian write_in, 679=Other Asian race combinations.
+    - RACED in the following list of values (630, 680, 682, 685, 689, 690, 698, 699), `race` = "Native Hawaiian or Other Pacific Islander". Note, these categories are: 630=Native Hawaiian, 680=Samoan, 682	Tongan, 685=Chamorro, 689=One or more other Micronesian races (2000,ACS), 690=Fijian, 699=Pacific Islander (PI), n.s.
+    - RACE = 7, `race` = "Other"
+    - RACE is either 8 or 9, `race` = "Two or More Races"
+4. Filter for `age` greater than 18
+5. Create `hispanic` variable = `FALSE` if HISPAN is "Not Hispanic", = `TRUE` if HISPAN is either ("Mexican", "Other", "Puerto Rican", "Cuban")
+6. Create `female` = `TRUE` if SEX == "Female", else `FALSE`
+7. Grouping by race, female, hispanic, and age, create the following summarized variables:
+  - `weight sum(PERWT)`
+  - `num = n()`
+  - Then recompute `weight`, outside the summarize so that it is a fraction of the total weight =  `weight / sum(weight)`
---
+++ code/download_ps_weights.R
@@ -0,0 +1,116 @@
+# Code to prepare population cell sizes for post-stratification
+# using ACS microdata
+
+library(askpass)
+library(dplyr)
+library(here)
+library(ipumsr)
+
+set_ipums_api_key(api_key = askpass("Please enter your API key"), save = FALSE)
+
+# Define variables in the extract
+usa_ext_def <- define_extract_micro(
+  collection = "usa",
+  description = "Extract for adaptive conjoint experiment",
+  samples = c("us2022a"),
+  variables = c("AGE", "SEX", "RACE", "STATEFIP", "HISPAN")
+)
+
+# Submit extract and wait for it to become ready
+usa_ext_submitted <- submit_extract(usa_ext_def)
+usa_ext_complete <- wait_for_extract(usa_ext_submitted)
+usa_ext_complete$status
+
+# Download extract and save it folder
+filepath <- download_extract(usa_ext_submitted, download_dir = tempdir())
+ddi <- read_ipums_ddi(filepath)
+micro_data <- read_ipums_micro(ddi)
+
+if (typeof(micro_data$AGE) != "integer") {
+  warning("Age variable is not an integer")
+}
+cat("\nMin, max values of age\n")
+min(micro_data$AGE)
+max(micro_data$AGE)
+
+# Codebook for ipums detailed race data
+# https://usa.ipums.org/usa-action/variables/RACE#codes_section
+df_race_form <- micro_data |>
+  # create race vars in ipums data to match acs
+  mutate(
+    race = case_when(
+      RACE == 1 ~ "White",
+      RACE == 2 ~ "Black or African American",
+      RACE == 3 ~ "American Indian or Alaska Native",
+      RACED %in% c(
+        400, 410, 420, 500, 600, 610, 620, 640,
+        641, 642, 643, 660, 661, 662, 663, 664,
+        665, 666, 667, 669, 670, 671, 673, 674,
+        675, 676, 677, 678, 679
+      ) ~ "Asian",
+      # A person having origins in any of the original peoples of Hawaii,
+      # Guam, Samoa, or other Pacific Islands. It includes people who
+      # indicate their race as “Native Hawaiian,” “Chamorro,”
+      # “Samoan,” and “Other Pacific Islander”
+      # or provide other detailed Pacific Islander responses such as
+      # Palauan, Tahitian, Chuukese, Pohnpeian, Saipanese, Yapese, etc.
+      RACED %in% c(
+        630, 680, 682, 685, 689, 690, 698, 699
+      ) ~ "Native Hawaiian or Other Pacific Islander",
+      RACE == 7 ~ "Other",
+      RACE %in% c(8, 9) ~ "Two or More Races",
+      TRUE ~ NA_character_
+    ),
+    race = factor(race, levels = c(
+      "Black or African American",
+      "White", "American Indian or Alaska Native",
+      "Asian", "Native Hawaiian or Other Pacific Islander",
+      "Other", "Two or More Races", "Prefer not to disclose"
+    ))
+  )
+
+# Check not missing (i.e., enumerated all categories)
+stopifnot(sum(is.na(df_race_form$race)) == 0)
+
+# check mapping from race (general) to new race var
+cat("\nDistinct values of RACE variable\n")
+df_race_form |>
+  distinct(RACE, race) |>
+  arrange(RACE) |>
+  table()
+
+# recode/fix other variables in ipums microdata
+cat("\nDistinct values of SEX variable\n")
+df_race_form |>
+  distinct(SEX)
+
+cat("\nDistinct values of HISPAN variable\n")
+df_race_form |>
+  distinct(HISPAN)
+
+df_wgt <- df_race_form |>
+  # create numeric age variable
+  mutate(age = as.numeric(AGE)) |>
+  assertr::verify(!is.na(age)) |>
+  filter(age >= 18) |>
+  mutate_if(is.labelled, as_factor) |>
+  # create hispanic or latino variable
+  mutate(hispanic = case_when(
+    HISPAN == "Not Hispanic" ~ FALSE,
+    HISPAN %in% c(
+      "Mexican", "Other",
+      "Puerto Rican", "Cuban"
+    ) ~ TRUE
+  )) |>
+  assertr::verify(!is.na(hispanic)) |>
+  # create female variable from sex
+  mutate(female = if_else(SEX == "Female", TRUE, FALSE)) |>
+  group_by(race, female, hispanic, age) |>
+  summarize(
+    weight = sum(PERWT),
+    num = n(),
+    .groups = "drop"
+  ) |>
+  mutate(weight = weight / sum(weight))
+
+saveRDS(df_wgt, here("data", "ipums_strata_sizes.RDS"))
