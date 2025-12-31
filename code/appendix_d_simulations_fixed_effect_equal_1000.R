e0f7f3be2015ad1295a8e161d76f2ae98c94826e
Jennah Gosciak <jennahgosciak@gmail.com>
2025-12-30 21:59:28 -0500

    add arrange

--- code/appendix_d_simulations_fixed_effect_equal_1000.R
+++ code/appendix_d_simulations_fixed_effect_equal_1000.R
@@ -1,23 +1,22 @@
-library(dplyr)
 library(here)
+library(dplyr)
 library(furrr)
 library(progressr)
+library(tidyr)
 
 ## Change this parameter to change the number of cores for parallelization
-n_cores <- parallel::detectCores()-1
+n_cores <- min(parallel::detectCores()-1, 120)
 plan(multisession, workers = n_cores)
 
 ## change this parameter to change the number of simulations
-n_sim <- 10
+n_sim <- 1000
 
-warmup_phase <- function(true_p, n_sim=10) {
-  pi_equal <- rep(1, length(true_p))/length(true_p)
+warmup_phase <- function(n_total, true_p) {
+  pi <- rep(1, length(true_p))/length(true_p)
   
-  n <- 0
-  max_reached <- FALSE
-  while (max_reached == FALSE) {
-    n <- n + 1
-    profile_draw <- rmultinom(1, 1, pi_equal)
+  n <- 1
+  while (n <= n_total) {
+    profile_draw <- rmultinom(1, 1, pi)
     context <- which.max(profile_draw)
     new_draw <- tibble(context = context,
                        outcome = rbinom(1,1, prob=true_p[context])) |> 
@@ -29,13 +28,40 @@ warmup_phase <- function(true_p, n_sim=10) {
     } else {
       df <- bind_rows(df, new_draw)
     }
+    n <- n + 1
+  }
+  outcomes <- df |> 
+    group_by(context) |>
+    summarize(y0 = sum(y0),
+              y1 = sum(y1)) |> 
+    ungroup()
+  
+  return(outcomes)
+}
+
+adaptive_phase <- function(true_p, n_sim=1000) {
+  print(length(true_p))
+  df <- warmup_phase(100, true_p)
+  
+  pi <- rep(1, length(true_p))/length(true_p)
+  
+  max_reached <- FALSE
+  n <- 1
+  while (max_reached == FALSE) {
+    profile_draw <- rmultinom(1, 1, pi)
+    context <- which.max(profile_draw)
+    adaptive_draw <- tibble(context = context,
+                            outcome = rbinom(1,1, prob=true_p[context])) |> 
+      mutate(y1 = if_else(outcome==1,1,0),
+             y0 = if_else(outcome==0,1,0))
+    
+    df <- bind_rows(df, adaptive_draw)
     
     if ((n %% 100) == 0) {
-      pi <- df |>
-        right_join(tibble(context = 1:length(true_p)), by = join_by(context)) |>
+      pi <- df |> 
         group_by(context) |> 
-        summarize(y1 = sum(y1, na.rm = TRUE),
-                  y0 = sum(y0, na.rm = TRUE)) |> 
+        summarize(y1 = sum(y1),
+                  y0 = sum(y0)) |> 
         mutate(theta_star = map2(.x = y1, .y = y0, 
                                  .f = \(x,y) rbeta(n_sim, 
                                                    x + 1, y + 1))) |> 
@@ -49,41 +75,53 @@ warmup_phase <- function(true_p, n_sim=10) {
         mutate(pi = n / n_sim) |> 
         right_join(tibble(max_arm = 1:length(true_p)), by = join_by(max_arm)) |>
         mutate(pi = if_else(is.na(pi),0,pi)) |> 
+        arrange(max_arm) |>
         pull(pi)
       
-      stopifnot(length(pi)==length(true_p))
       if (max(pi) >= 0.95) {
         max_reached <- TRUE
       }
+    } else {
+      pi <- df |> 
+        group_by(context) |> 
+        summarize(y1 = sum(y1),
+                  y0 = sum(y0)) |> 
+        right_join(tibble(context = 1:length(true_p)), by = join_by(context)) |>
+        mutate(y1 = if_else(is.na(y1),0,y1),
+               y0 = if_else(is.na(y0),0,y0)) |> 
+        mutate(theta_star = rbeta(n(), y1 + 1, y0 + 1)) |> 
+        arrange(context) |> 
+        mutate(max_arm = as.numeric(context == which.max(theta_star))) |>
+        pull(max_arm)
     }
+    stopifnot(abs(sum(pi)-1) < 0.05)
+    stopifnot(length(pi)==length(true_p))
+    n <- n + 1
   }
   outcomes <- df |> 
-    right_join(tibble(context = 1:length(true_p)), by = join_by(context)) |>
     group_by(context) |>
-    summarize(y0 = sum(y0, na.rm = TRUE),
-              y1 = sum(y1, na.rm = TRUE)) |> 
+    summarize(y0 = sum(y0),
+              y1 = sum(y1)) |> 
     ungroup() |> 
-    arrange(context) |> 
     cbind(pi) |> 
     mutate(n_total = n,
-           num_arms = length(true_p)) |> 
-    mutate(chose_correct = if_else(num_arms == which.max(pi), 1, 0))
+           num_arms = length(true_p))
+  
   return(outcomes)
 }
 
 set.seed(1234)
 run_with_progress <- function(n_sim, n_arms) {
   true_p <- c(seq(0.3, 0.65, length.out = n_arms-1), 0.7)
-  print(length(true_p))
   
   with_progress({
     # Initialize a progressor
     p <- progressor(steps = length(n_sim))
-
+    
     res <- future_map_dfr(1:n_sim, ~{
-      # p()
+      #p()
       Sys.sleep(0.05)
-      warmup_phase(true_p)
+      adaptive_phase(true_p)
     },.options=furrr_options(seed=TRUE))
   })
   return(res)
@@ -100,4 +138,4 @@ outcomes <- run_with_progress(n_sim, 9) |>
 outcomes
 
 outcomes |>
-  saveRDS(here("data/simulation-data/fixed_effect_equal_sim_1000.RDS"))
+  saveRDS(here("data/simulation-data/fixed_effect_adaptive_sim_1000.RDS"))
